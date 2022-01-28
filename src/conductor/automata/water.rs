4d4f2faa2e8b8f1b25254d171b2c9f8cc47e951c
kirinokirino <kirinokirino@lunariko.com>
2022-01-28 18:50:49 +0300

    Coloring and stuff before uploading.

--- src/conductor/automata/water.rs
+++ src/conductor/automata/water.rs
@@ -3,5 +3,5 @@ use crate::common::Position;
 use fastrand;
 
 pub fn update(pos: &Position) -> Destination {
-    Destination::from(*pos + Position::new(fastrand::i64(-1..2), 1))
+    Destination::from(*pos + Position::new(fastrand::i64(-1..2), fastrand::i64(0..2)))
 }
